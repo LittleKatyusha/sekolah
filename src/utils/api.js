4a530192988dc816615abb14a39e68b50b628116
maspriyambodo <maspriyambodo>
2026-02-28 00:31:55 +0700

    remove header Origin

--- src/utils/api.js
+++ src/utils/api.js
@@ -13,7 +13,6 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
-    'Origin': window.location.origin, // Set origin header for CORS
   },
 })
 
